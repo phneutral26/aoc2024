bab304174403b2ac367c686f6f99eb0a62c6e376
Phil <philerichartmann@gmail.com>
2024-12-09 11:35:07 +0100

    dec07 lösung

---
+++ Dec_07/find_correct_equations.py
@@ -0,0 +1,32 @@
+import sys
+import re
+from collections import defaultdict, Counter, deque
+
+sys.setrecursionlimit(10**6)
+infile = sys.argv[1] if len(sys.argv)>=2 else 'input'
+p1 = 0
+p2 = 0
+D = open(infile).read().strip()
+
+def is_valid(target, ns, p2):
+    if len(ns) == 1:
+        return ns[0]==target
+    if is_valid(target, [ns[0]+ns[1]] + ns[2:], p2):
+        return True
+    if is_valid(target, [ns[0]*ns[1]] + ns[2:], p2):
+        return True
+    if p2 and is_valid(target, [int(str(ns[0])+str(ns[1]))] + ns[2:], p2):
+        return True
+    return False
+
+for line in D.strip().split('\n'):
+    target, ns = line.strip().split(':')
+    target = int(target)
+    ns = [int(x) for x in ns.strip().split()]
+    if is_valid(target, ns, p2=False):
+        p1 += target
+    if is_valid(target, ns, p2=True):
+        p2 += target
+
+print(p1)
+print(p2)
